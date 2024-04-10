249f9a668ba8eb3c5f9e219e81bf1cdf62e12ed0
Mello Birkan <mgbirkan@icloud.com>
2024-04-10 17:09:09 -0300

    First Feature

---
+++ TCADemo/CounterFeature.swift
@@ -0,0 +1,76 @@
+//
+//  CounterFeature.swift
+//  TCADemo
+//
+//  Created by Marcello Gonzatto Birkan on 10/04/24.
+//
+
+import ComposableArchitecture
+import SwiftUI
+
+
+struct CounterView: View {
+  let store: StoreOf<CounterFeature>
+  
+  var body: some View {
+    VStack {
+          Text("\(store.count)")
+            .font(.largeTitle)
+            .padding()
+            .background(Color.black.opacity(0.1))
+            .cornerRadius(10)
+          HStack {
+            Button("-") {
+              store.send(.decrementButtonTapped)
+            }
+            .font(.largeTitle)
+            .padding()
+            .background(Color.black.opacity(0.1))
+            .cornerRadius(10)
+            
+            Button("+") {
+              store.send(.incrementButtonTapped)
+            }
+            .font(.largeTitle)
+            .padding()
+            .background(Color.black.opacity(0.1))
+            .cornerRadius(10)
+          }
+        }
+  }
+}
+
+#Preview {
+  CounterView(
+    store: Store(initialState: CounterFeature.State()) {
+      CounterFeature()
+    }
+  )
+}
+
+@Reducer
+struct CounterFeature {
+  @ObservableState
+  struct State {
+    var count = 0
+  }
+  
+  enum Action {
+    case decrementButtonTapped
+    case incrementButtonTapped
+  }
+  
+  var body: some ReducerOf<Self> {
+    Reduce { state, action in
+      switch action {
+      case .decrementButtonTapped:
+        state.count -= 1
+        return .none
+        
+      case .incrementButtonTapped:
+        state.count += 1
+        return .none
+      }
+    }
+  }
+}
--- TCADemo/TCADemoApp.swift
+++ TCADemo/TCADemoApp.swift
@@ -4,14 +4,18 @@
 //
 //  Created by Marcello Gonzatto Birkan on 10/04/24.
 //
-
+import ComposableArchitecture
 import SwiftUI
 
 @main
 struct TCADemoApp: App {
+  static let store = Store(initialState: CounterFeature.State()) {
+      CounterFeature()
+    }
+  
     var body: some Scene {
         WindowGroup {
-            ContentView()
+          CounterView(store: TCADemoApp.store)
         }
     }
 }
