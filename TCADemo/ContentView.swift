aab2e0af6c4a2730aec0a12cef016bbc91b45a29
Mello Birkan <mgbirkan@icloud.com>
2024-04-10 16:32:32 -0300

    Initial Commit

---
+++ TCADemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TCADemo
+//
+//  Created by Marcello Gonzatto Birkan on 10/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TCADemo/TCADemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCADemoApp.swift
+//  TCADemo
+//
+//  Created by Marcello Gonzatto Birkan on 10/04/24.
+//
+
+import SwiftUI
+
+@main
+struct TCADemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
